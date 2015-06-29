985a0533f3437a986f7cbfeaccdc8b3b74928ba4
Deavon M. McCaffery <dmccaffery@pulsebridge.com>
2015-06-28 18:24:45 -0700

    Added support for emmitting the commit id and repository URI to the assembly info (by parsing git config).
    Related Work Items: #4, #8

--- test/src/condo.project/Properties/AssemblyInfo.cs
+++ test/src/condo.project/Properties/AssemblyInfo.cs
@@ -8,11 +8,12 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.62915")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha-062915-0103")]
+[assembly: AssemblyInformationalVersion("1.0.0-alpha-062915-0124")]
 
 #if !PORTABLE
-[assembly: AssemblyMetadata("BuildDateUtc", @"2015-06-29T01:03:49.809Z")]
-[assembly: AssemblyMetadata("Branch", @"refs/heads/feature/3-version")]
-[assembly: AssemblyMetadata("CommitId", @"176494fb514a55b38fd9d1e42a02e8c67173e5b4")]
+[assembly: AssemblyMetadata("BuildDateUtc", @"2015-06-29T01:24:19.245Z")]
+[assembly: AssemblyMetadata("Branch", @"refs/heads/feature/4-commitid")]
+[assembly: AssemblyMetadata("CommitId", @"9c0f0365c5738ca392dcd9a2b316de4855be8a14")]
 [assembly: AssemblyMetadata("BuildId", @"062915")]
+[assembly: AssemblyMetadata("RepositoryUri", @"https://dmccaffery-pb.visualstudio.com/defaultcollection/open/_git/condo")]
 #endif
