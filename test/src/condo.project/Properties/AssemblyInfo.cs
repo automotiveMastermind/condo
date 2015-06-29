aca6edb27fd7430a2da033e49fb58a9c7e6bea17
Deavon M. McCaffery <dmccaffery@pulsebridge.com>
2015-06-28 18:06:44 -0700

    Added support for generating version information for both local and server builds.
    Related Work Items: #3

--- test/src/condo.project/Properties/AssemblyInfo.cs
+++ test/src/condo.project/Properties/AssemblyInfo.cs
@@ -7,8 +7,12 @@
 using System.Reflection;
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0--1")]
+[assembly: AssemblyFileVersion("1.0.0.62915")]
+[assembly: AssemblyInformationalVersion("1.0.0-alpha-062915-0103")]
+
 #if !PORTABLE
-[assembly: AssemblyMetadata("BuildDateUtc", @"2015-06-28T07:29:52.089Z")]
+[assembly: AssemblyMetadata("BuildDateUtc", @"2015-06-29T01:03:49.809Z")]
+[assembly: AssemblyMetadata("Branch", @"refs/heads/feature/3-version")]
+[assembly: AssemblyMetadata("CommitId", @"176494fb514a55b38fd9d1e42a02e8c67173e5b4")]
+[assembly: AssemblyMetadata("BuildId", @"062915")]
 #endif
