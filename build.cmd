5abc05260526d541db4875ccf872e161c610bc32
Deavon M. McCaffery <dmccaffery@pulsebridge.com>
2015-07-03 12:48:47 -0700

    CLI build is now working (at least locally).
    Related Work Items: #15

--- build.cmd
+++ build.cmd
@@ -5,20 +5,26 @@ SETLOCAL
     SET DNXPATH=%USERPROFILE%\.dnx
     SET DNVMPATH=%DNXPATH%\dnvm
     SET DNVMCMD=%DNVMPATH%\dnvm.cmd
-    SET DNVMURI="https://raw.githubusercontent.com/aspnet/Home/dev/dnvm.cmd"
+    SET DNVMPS1=%DNVMPATH%\dnvm.ps1
+    SET DNVMCMDURI="https://raw.githubusercontent.com/aspnet/Home/dev/dnvm.cmd"
+    SET DNVMPS1URI="https://raw.githubusercontent.com/aspnet/Home/dev/dnvm.ps1"
 
     IF NOT EXIST "%DNVMPATH%" (
         mkdir "%DNVMPATH%"
     )
 
+    IF NOT EXIST "%DNVMPS1%" (
+	@powershell -NoProfile -ExecutionPolicy Unrestricted -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest '%DNVMPS1URI%' -OutFile '%DNVMPS1%' -UseBasicParsing"
+    )
+
     IF NOT EXIST "%DNVMCMD%" (
-        @powershell -NoProfile -ExecutionPolicy Unrestricted -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest '%DNVMURI%' -OutFile '%DNVMCMD%'"
+        @powershell -NoProfile -ExecutionPolicy Unrestricted -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest '%DNVMCMDURI%' -OutFile '%DNVMCMD%' -UseBasicParsing"
 
         CALL "%DNVMCMD%" update-self
     )
 
-    CALL "%DNVMCMD%" upgrade -runtime CLR -arch x86
-    CALL "%DNVMCMD%" upgrade -runtime CoreCLR -arch x86
+    CALL "%DNVMCMD%" upgrade -runtime CLR -arch x86 -ngen
+    CALL "%DNVMCMD%" upgrade -runtime CoreCLR -arch x86 -ngen
     CALL "%DNVMCMD%" use default -runtime CLR -arch x86
 
     SET NUGETCMD=%LOCALAPPDATA%\NuGet\nuget.exe
@@ -46,6 +52,6 @@ SETLOCAL
     IF NOT EXIST "%SAKE%" (
         "%NUGET%" install Sake -pre -o packages -ExcludeVersion
     )
-    
+
     "%SAKE%" -I "%INCLUDES%" -f "%MAKE%" %*
 ENDLOCAL
