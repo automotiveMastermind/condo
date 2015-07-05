054afe087b9f6611f353ce0713436a439a698ef0
Deavon M. McCaffery <dmccaffery@pulsebridge.com>
2015-07-04 20:15:06 -0700

    Updated the paths for the base path and target path to use TFS Build variables (if available).

--- test/condo.project.test/Properties/AssemblyInfo.cs
+++ test/condo.project.test/Properties/AssemblyInfo.cs
@@ -8,13 +8,13 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.70515")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha-070515-0247")]
+[assembly: AssemblyInformationalVersion("1.0.0-alpha-070515-0314")]
 #if !PORTABLE
 [assembly: AssemblyMetadata("Branch", @"refs/heads/feature/12-buildtests")]
 [assembly: AssemblyMetadata("BuildId", @"070515")]
-[assembly: AssemblyMetadata("CommitId", @"4ac091661e6c9c56b11802b637088a01b7bba6ab")]
-[assembly: AssemblyMetadata("BuildDateUtc", @"2015-07-05T02:47:38.248Z")]
+[assembly: AssemblyMetadata("CommitId", @"ad494dd9deb1f2e990747b1df1a3646364cf1410")]
+[assembly: AssemblyMetadata("BuildDateUtc", @"2015-07-05T03:14:44.188Z")]
 [assembly: AssemblyMetadata("BuiltBy", @"dmccaffery@pulsebridge.com")]
-[assembly: AssemblyMetadata("BuiltOn", @"DEAVONMMCCA5FCC")]
+[assembly: AssemblyMetadata("BuiltOn", @"gio-ws-01.local")]
 [assembly: AssemblyMetadata("RepositoryUri", @"https://dmccaffery-pb.visualstudio.com/defaultcollection/open/_git/condo")]
 #endif
