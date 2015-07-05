4ac091661e6c9c56b11802b637088a01b7bba6ab
Deavon M. McCaffery <dmccaffery@pulsebridge.com>
2015-07-04 19:46:51 -0700

    No longer using "dnvm run" on Windows, because it doesn't seem to be available in all flavors of Beta 5.

--- test/condo.project.test/Properties/AssemblyInfo.cs
+++ test/condo.project.test/Properties/AssemblyInfo.cs
@@ -8,13 +8,13 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.70515")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha-070515-0237")]
+[assembly: AssemblyInformationalVersion("1.0.0-alpha-070515-0243")]
 #if !PORTABLE
 [assembly: AssemblyMetadata("Branch", @"refs/heads/feature/12-buildtests")]
 [assembly: AssemblyMetadata("BuildId", @"070515")]
-[assembly: AssemblyMetadata("CommitId", @"2d89efa9fa0c1f160a42c0d8dee2e6ff035dfbe1")]
-[assembly: AssemblyMetadata("BuildDateUtc", @"2015-07-05T02:37:24.162Z")]
+[assembly: AssemblyMetadata("CommitId", @"5dc7605d5524cf07ba971293f03908bda95f7549")]
+[assembly: AssemblyMetadata("BuildDateUtc", @"2015-07-05T02:43:41.588Z")]
 [assembly: AssemblyMetadata("BuiltBy", @"dmccaffery@pulsebridge.com")]
-[assembly: AssemblyMetadata("BuiltOn", @"gio-ws-01.local")]
+[assembly: AssemblyMetadata("BuiltOn", @"DEAVONMMCCA5FCC")]
 [assembly: AssemblyMetadata("RepositoryUri", @"https://dmccaffery-pb.visualstudio.com/defaultcollection/open/_git/condo")]
 #endif
