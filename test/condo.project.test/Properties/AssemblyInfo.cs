5dc7605d5524cf07ba971293f03908bda95f7549
Deavon M. McCaffery <dmccaffery@pulsebridge.com>
2015-07-04 19:39:17 -0700

    Updated the logging to be more consistent throughout the includes.
    Added support for executing DNX commands (either through dnvm run on Windows, or directly through dnx on POSIX).
    
    Updated NuGet include to rely on DNX for execution (instead of calling dnx directly).
    
    Added support for executing unit tests on DNX-based projects and outputing test results XML to artifacts folder (currently, this only supports xunit).
    
    Created an example Fact and Theory for use in proving XUnit is executing correctly.
    
    Related Work Items: #12

--- test/condo.project.test/Properties/AssemblyInfo.cs
+++ test/condo.project.test/Properties/AssemblyInfo.cs
@@ -9,10 +9,12 @@ using System.Reflection;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.70515")]
 [assembly: AssemblyInformationalVersion("1.0.0-alpha-070515-0237")]
-[assembly: AssemblyMetadata("BuildDateUtc", @"2015-07-05T02:37:24.162Z")]
+#if !PORTABLE
 [assembly: AssemblyMetadata("Branch", @"refs/heads/feature/12-buildtests")]
 [assembly: AssemblyMetadata("BuildId", @"070515")]
 [assembly: AssemblyMetadata("CommitId", @"2d89efa9fa0c1f160a42c0d8dee2e6ff035dfbe1")]
+[assembly: AssemblyMetadata("BuildDateUtc", @"2015-07-05T02:37:24.162Z")]
 [assembly: AssemblyMetadata("BuiltBy", @"dmccaffery@pulsebridge.com")]
 [assembly: AssemblyMetadata("BuiltOn", @"gio-ws-01.local")]
 [assembly: AssemblyMetadata("RepositoryUri", @"https://dmccaffery-pb.visualstudio.com/defaultcollection/open/_git/condo")]
+#endif
