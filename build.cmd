5707d6330ff4b3d125bd99226183c4ba441001d9
Deavon M. McCaffery <dmccaffery@pulsebridge.com>
2015-11-22 15:54:34 -0800

    Use a specific version of the runtime for DNU and DNX commands

--- build.cmd
+++ build.cmd
@@ -54,8 +54,8 @@ SETLOCAL
         copy "%NUGETCMD%" "%NUGET%"
     )
 
-    CALL "%DNVMCMD%" install latest -nonative
-    CALL "%DNVMCMD%" install latest -r coreclr -nonative
+    CALL "%DNVMCMD%" install latest -nonative -alias default
+    CALL "%DNVMCMD%" install latest -r coreclr -nonative -alias default
 
     SET SAKEPKG=packages\Sake
     SET SAKE=%SAKEPKG%\tools\Sake.exe
