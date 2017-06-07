649aeb144e7f9c7676a7ac3cc6729deb36252040
Brandon Hay <brandon.a.hay@gmail.com>
2017-06-07 16:44:42 -0400

    perf(nuget restore): disable xml generation on nuget restore

--- src/AM.Condo/Scripts/condo.ps1
+++ src/AM.Condo/Scripts/condo.ps1
@@ -65,6 +65,9 @@ function Get-File([string] $url, [string] $path, [int] $retries = 5) {
 # disable dotnet cli telemetry to speed up the build
 $env:DOTNET_CLI_TELEMETRY_OPTOUT = 1
 
+# disable xml creation on nuget restore to speed up the build
+$env:NUGET_XMLDOC_MODE = "skip"
+
 # set well-known paths
 $WorkingPath = Convert-Path (Get-Location)
 
